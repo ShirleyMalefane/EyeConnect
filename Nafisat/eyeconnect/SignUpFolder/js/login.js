62f39269e06de443e2ebac482f67e8527f920caf
Nafisat Abdulrasaq <148299521+nafisatA@users.noreply.github.com>
2024-04-06 09:39:06 -0700

    pages were made responsive
    login, signup and otp functionalities were added

---
+++ Nafisat/eyeconnect/SignUpFolder/js/login.js
@@ -0,0 +1,35 @@
+const apiEntry = "https://eyeconnect-api.onrender.com";
+
+const loginBtn= document.getElementById("login-button")
+const emailInp=document.getElementById("email")
+const passwordInp=document.getElementById("password")
+
+const login=()=>{
+    loginBtn.innerHTML="Loading..."
+    loginBtn.disabled=true
+    const all_inputs=[emailInp,passwordInp];
+    const all_inputs_were_filled=all_inputs.every(inp=>inp.value.length>0)
+    if(!all_inputs_were_filled){
+        alert("Completely fill out all fields to continue")
+    }
+    else{
+        fetch(`${apiEntry}/users/login`, {
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email:emailInp.value,
+                password:passwordInp.value
+            })
+        }).then(res=>res.json()).then(data=>{
+            if(data.success){
+                alert( `successful login, youre now logged in as ${data.result.name}`)
+            }
+             else{
+                alert(data.result)
+                window.location.reload()
+            }
+        })
+    }
+}
